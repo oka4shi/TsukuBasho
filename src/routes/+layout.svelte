3a18694d0f37aacc4bbab44b4c84ea2afffb083e
OKAMOTO Shigehiro <primary@oka4shi.dev>
2025-03-31 18:17:49 +0900

    implement LP(wip)

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,15 @@
 <script lang="ts">
-    let { children } = $props();
+  import "./styles.css";
+
+  let { children } = $props();
 </script>
 
-{@render children()}
+<div class="app">
+  <main>
+    {@render children()}
+  </main>
+
+  <footer>
+    <p>筑波大学教室確認支援システム TsukuBasho - 2025 おかし</p>
+  </footer>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,19 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+  import FileInput from "$lib/FileInput.svelte";
+</script>
+
+<h1>TsukuBasho（ベータ版）</h1>
+
+<h2>初回のデータ登録を行ってください</h2>
+<p>
+  大学公式の資料
+  <a
+    href="https://www.tsukuba.ac.jp/education/pdf/how-to-check-the-classrooms-to-be-used-for-courses.pdf"
+    >「授業で使用する教室の確認方法」</a
+  >を参考にダウンロードしたExcelファイルを登録してください
+</p>
+<p>
+  なお、データは完全に端末の中で処理され、インターネットにアップロードされることは一切ありません
+</p>
+
+<FileInput />
